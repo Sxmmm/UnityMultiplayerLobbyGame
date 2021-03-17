ce6a71351b8af11db457b8ea097fdde2b69315ba
Sam Baker <09sam@live.co.uk>
2021-03-17 20:48:27 +0000

    P2P Player networking working nicely

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : NetworkBehaviour
+{
+    private void handleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            float moverHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moverHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+    private void Update()
+    {
+        handleMovement();
+    }
+}
